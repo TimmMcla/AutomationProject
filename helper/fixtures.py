b43a2e2caa9224b72269df46cbfc132621e89748
Timm McLaughlin <timmothymclaughlin@gmail.com>
2021-07-13 02:27:19 -0500

    removed data

--- helper/fixtures.py
+++ helper/fixtures.py
@@ -4,4 +4,4 @@ from helper.driver_helper import DriverHelper
 
 def get_browser(browser):
     if browser == "chrome":
-        return DriverHelper(webdriver.Chrome("/home/timm/chromedriver_linux64/chromedriver"))
+        return DriverHelper(webdriver.Chrome(""))
